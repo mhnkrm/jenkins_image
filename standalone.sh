7a2e419b5a57f1d64c31b5cb5eb4d7a477a3c6eb
mhnkrm <mhn.krm@gmail.com>
2022-09-13 18:23:29 +0530

    Update standalone.sh

--- standalone.sh
+++ standalone.sh
@@ -4,10 +4,17 @@ if [ "${prov}" = False ] ; then
     export port_prov=False
 fi
 
+
 git clone https://github.com/mhnkrm/warriorframework.git
 repo project init -u testcase-manifests t900-1.0
 repo project sync
 sed -i s"|warriorframework==4.4.0|${WORKSPACE}/${folder}/warriorframework/warriorframework-4.4.0-py3-none-any.whl|" ${WORKSPACE}/${folder}/test_framework/warrior-keywords/1finity/conf/warrior-requirements.pip
 sed -i s"|output_file_obj.write(stamp + ' ' + type + ':' + job_obj.name + ' # ' + line)|output_file_obj.write(line) if 'Warrior' in job_obj.name else output_file_obj.write(job_obj.name + ' # ' + line)|" ${WORKSPACE}/${folder}/test_framework/network-topology/bin/exec-job-tree.py
 cd ${WORKSPACE}/${folder}/test_framework
+
+card=$(python -c 'import os,re;job_url=os.getenv("JOB_URL");print(re.findall(r"(P9.*?)/job/",job_url)[0])') 
+export card_type=PIT9-$card
+export product=T900/$card
+
+
 
