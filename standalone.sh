4df380516e6673bfb41cb71dff1f2082f8b59acf
mhnkrm <mhn.krm@gmail.com>
2022-09-13 17:18:42 +0530

    Update standalone.sh

--- standalone.sh
+++ standalone.sh
@@ -10,5 +10,6 @@ repo project sync
 sed -i s"|warriorframework==4.4.0|${WORKSPACE}/${folder}/warriorframework/warriorframework-4.4.0-py3-none-any.whl|" ${WORKSPACE}/${folder}/test_framework/warrior-keywords/1finity/conf/warrior-requirements.pip
 sed -i s"|output_file_obj.write(stamp + ' ' + type + ':' + job_obj.name + ' # ' + line)|output_file_obj.write(line) if 'Warrior' in job_obj.name else output_file_obj.write(job_obj.name + ' # ' + line)|" ${WORKSPACE}/${folder}/test_framework/network-topology/bin/exec-job-tree.py
 cd ${WORKSPACE}/${folder}/test_framework
+pwd
 source ./fnc-init-test-env
 
